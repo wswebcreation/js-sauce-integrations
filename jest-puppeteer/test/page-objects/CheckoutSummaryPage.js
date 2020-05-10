f94c6fc59d8f789dbefb0470a48616609c5e9db1
Wim Selles <wim@wswebcreation.nl>
2020-05-10 20:47:44 +0200

    test: add checkout summary and complete tests

--- jest-puppeteer/test/page-objects/CheckoutSummaryPage.js
+++ jest-puppeteer/test/page-objects/CheckoutSummaryPage.js
@@ -1,9 +1,6 @@
 const BasePage = require('./BasePage');
 const SELECTORS = {
     screen: '#checkout_summary_container',
-    title: '.inventory_item_name',
-    description: '.inventory_item_desc',
-    price: '.inventory_item_price',
     cancelButton: '.cart_cancel_link',
     finishButton: '.cart_button',
     items: '.cart_item',
@@ -12,6 +9,49 @@ const SELECTORS = {
 class CheckoutSummaryPage extends BasePage {
     constructor() {
         super(SELECTORS.screen);
+    }
+
+    /**
+     * Get the amount of swag items in the cart
+     *
+     * @returns {Promise<number>}
+     */
+    async getSwagAmount() {
+        return (await page.$$(SELECTORS.items)).length;
+    }
+
+    /**
+     * Get a swag Item based on a search string or a number of the visible items
+     *
+     * @param {number|string} needle
+     *
+     * @return {Promise<ElementHandle>} the selected swag
+     */
+    async swag(needle) {
+        if (typeof needle === 'string') {
+            return page.evaluateHandle(
+                (items, string) =>
+                    [...document.querySelectorAll(items)].find(item => item.textContent.includes(string)),
+                SELECTORS.items,
+                needle
+            );
+        }
+
+        return (await page.$$(SELECTORS.items))[needle];
+    }
+
+    /**
+     * Cancel checkout
+     */
+    async cancelCheckout() {
+        await page.click(SELECTORS.cancelButton);
+    }
+
+    /**
+     * Finish checkout
+     */
+    async finishCheckout() {
+        await page.click(SELECTORS.finishButton);
     }
 }
 
